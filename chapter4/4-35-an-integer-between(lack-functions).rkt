afd02bbad6924207a7edb14aa16a7b3999bcee07
blue-jay <whudhn@gmail.com>
2015-07-23 20:24:10 +0800

    ch4
    4-35

---
+++ chapter4/4-35-an-integer-between(lack-functions).rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (an-integer-between i j)
+  (amb a (an-integer-between (+ i 1) j)))
+
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+    (let ((j (an-integer-between i high)))
+      (let ((k (an-integer-between j high)))
+        (require (= (+ (* i i) (* j j)) (* k k)))
+        (list i j k)))))
