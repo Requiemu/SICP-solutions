e6a58059a7f526d4bdec0d3629f528d7be03c391
Haonan Deng <whudhn@gmail.com>
2015-07-30 09:54:15 +0800

    ch4

--- chapter4/4-43.rkt
+++ chapter4/4-43.rkt
@@ -2,7 +2,7 @@
 (define (person name daughter yacht)
   (list name daughter yacht))
 
-(define Moore (person #f Lorna))
+(define Moore (person #f 'Lorna))
 (define Colonel (person #f 'Melissa))
 (define Hall (person #f 'Rosalind))
 (define Barnacle (person 'Melissa 'Gabrielle))
@@ -17,5 +17,30 @@
 ;;(require (eq? (yacht (father Gabrielle))
 ;;              (daughter Parker))
 
-(define (solutions
-  
+(define (list-except l n)
+  (cond ((null? l) '())
+        ((eq? n (car l)) (list-except (cdr l) n))
+        ((else (cons (car l) (list-except (cdr l) n))))))
+
+(define (one-of-yachts l)
+  (apply amb (map caddr l)))
+
+(define (one-of-daughters l)
+  (apply amb (map cadr l)))
+
+(define (solutions)
+  (set! (daughter Moore) (one-of-yachts
+                          (list-except fathers Moore)))
+  (set! (daughter Colonel) (one-of-yachts
+                            (list-except fathers Colonel)))
+  (set! (daughter Hall) (one-of-yachts
+                         (list-except fathers Hall)))
+  (let ((unknow-father) (apply amb fathers))
+    (require (and (eq? (daughter unknown-father) 'Gabrielle)
+                  (eq? (yacht unknown-father) (daughter Parker))))
+    (list Moore Colonel Hall Barnacle Parker)))
+    
+    
+        
+        
+        
