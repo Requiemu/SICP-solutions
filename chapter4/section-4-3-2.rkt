f09fc596e4ce898661eb094b6c3af8d87a7ad7ea
blue-jay <whudhn@gmail.com>
2015-08-17 18:35:52 +0800

    save

--- chapter4/section-4-3-2.rkt
+++ chapter4/section-4-3-2.rkt
@@ -5,18 +5,20 @@
 
 (define articles '(article the a))
 
+(define prepositions '(prep for to in by with)) 
+
 ;(sentence (noun-phrase (article the) (noun cat))
 ;          (verb eats))
 
-(define (parse-sentence)
-  (list 'sentence
-        (parse-noun-phrase)
-        (parse-word verbs)))
+;(define (parse-sentence)
+;  (list 'sentence
+;        (parse-noun-phrase)
+;        (parse-word verbs)))
 
-(define (parse-noun-phrase)
-  (list 'noun-phrase
-        (parse-word articles)
-        (parse-word nouns)))
+;(define (parse-noun-phrase)
+;  (list 'noun-phrase
+;        (parse-word articles)
+;        (parse-word nouns)))
 
 (define (parse-word word-list)
   (require (not (null? *unparsed*)))
@@ -40,7 +42,8 @@
 
 (define (parse-sentence)
   (list 'sentence
-        (parse-noun-phrase)))
+        (parse-noun-phrase)
+        (parse-verb-phrase)))
 
 (define (parse-verb-phrase)
   (define (maybe-extend verb-phrase)
@@ -49,4 +52,17 @@
                              verb-phrase
                              (parse-prepositional-phrase)))))
   (maybe-extend (parse-word verbs)))
+
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+         (maybe-extend (list 'noun-phrase
+                             noun-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
                        
